4fa8de53179b39b8a9cbe30470d4afcf3af8ec9b
Gary <gary@garys-macbook-pro.local>
2018-08-29 20:12:06 +0700

    Minor fix

---
+++ bookchecker/Networking/APIRequest.swift
@@ -0,0 +1,29 @@
+//
+//  APIRequest.swift
+//  bookchecker
+//
+//  Created by Gary on 8/29/18.
+//  Copyright © 2018 Gary. All rights reserved.
+//
+
+import Foundation
+
+struct APIRequest {
+	var source: APISource
+	var searchURL = ""
+	var metadataURL = ""
+	var downloadURL = ""
+
+	init(source: APISource) {
+		self.source = source
+		switch source {
+		case .google:
+			searchURL = "https://www.googleapis.com/books/v1/volumes"
+		case .archive:
+			searchURL = ""
+			metadataURL = "https://archive.org/metadata"
+			downloadURL = "https://archive.org/download"
+		}
+	}
+}
+
---
+++ bookchecker/Networking/APISource.swift
@@ -0,0 +1,13 @@
+//
+//  APISource.swift
+//  bookchecker
+//
+//  Created by Gary on 8/29/18.
+//  Copyright © 2018 Gary. All rights reserved.
+//
+
+import Foundation
+
+enum APISource {
+	case google, archive
+}
